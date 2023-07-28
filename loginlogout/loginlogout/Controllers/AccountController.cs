f4fe00cb7db4c294533f8adba51139c7a3d08b10
MD. FORHADUL ISLAM <forhadislam110@gmail.com>
2023-07-28 12:28:54 +0600

    LogINlogOut

---
+++ loginlogout/loginlogout/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using loginlogout.Models;
+using System.Web.Security;
+
+namespace loginlogout.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Membeship model)
+        {
+            using (var context = new PRODUCTEntities())
+            {
+                bool isValid = context.Regi.Any(x=>x.UserName == model.UserName && x.Password == model.Password);
+                if(isValid)
+                {
+
+                    FormsAuthentication.SetAuthCookie(model.UserName,false);
+                    return RedirectToAction("Index","PRODUCT_TABLE");
+
+                }
+                ModelState.AddModelError("", "Invalid UserName and Password");
+                    return View();
+            }
+               
+        }
+
+        //for singUP
+        public ActionResult SingUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SingUp(Regi m)
+        {
+            using (var context = new PRODUCTEntities())
+            {
+                context.Regi.Add(m);
+                context.SaveChanges();
+            }
+                return RedirectToAction("Login");
+        }
+
+        //Logout 
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+
+        }
+    }
+}
---
+++ loginlogout/loginlogout/Controllers/PRODUCT_TABLEController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using loginlogout;
+
+namespace loginlogout.Controllers
+{
+    [Authorize]
+    public class PRODUCT_TABLEController : Controller
+    {
+        private PRODUCTEntities db = new PRODUCTEntities();
+
+        // GET: PRODUCT_TABLE
+        public ActionResult Index()
+        {
+            return View(db.PRODUCT_TABLE.ToList());
+        }
+
+        // GET: PRODUCT_TABLE/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT_TABLE pRODUCT_TABLE = db.PRODUCT_TABLE.Find(id);
+            if (pRODUCT_TABLE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT_TABLE);
+        }
+
+        // GET: PRODUCT_TABLE/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PRODUCT_TABLE/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Price,Exdate")] PRODUCT_TABLE pRODUCT_TABLE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PRODUCT_TABLE.Add(pRODUCT_TABLE);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pRODUCT_TABLE);
+        }
+
+        // GET: PRODUCT_TABLE/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT_TABLE pRODUCT_TABLE = db.PRODUCT_TABLE.Find(id);
+            if (pRODUCT_TABLE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT_TABLE);
+        }
+
+        // POST: PRODUCT_TABLE/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Price,Exdate")] PRODUCT_TABLE pRODUCT_TABLE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pRODUCT_TABLE).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pRODUCT_TABLE);
+        }
+
+        // GET: PRODUCT_TABLE/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT_TABLE pRODUCT_TABLE = db.PRODUCT_TABLE.Find(id);
+            if (pRODUCT_TABLE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT_TABLE);
+        }
+
+        // POST: PRODUCT_TABLE/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PRODUCT_TABLE pRODUCT_TABLE = db.PRODUCT_TABLE.Find(id);
+            db.PRODUCT_TABLE.Remove(pRODUCT_TABLE);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
